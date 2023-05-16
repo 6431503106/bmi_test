23370d9fe6309982ac5f4896db845e4222646bb5
6431503106 <6431503106@lamduan.mfu.ac.th>
2023-05-16 16:20:29 +0700

    Srk

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,7 @@ import 'package:bmi_test/screen/welcome.dart';
 
 import 'package:firebase_core/firebase_core.dart';
 
-Future main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+void main() {
   runApp(MyApp());
 }
 
